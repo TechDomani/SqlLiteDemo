95143cc5a78dd0c01e630b3d33d1886e83d269ea
TechDomani <73493660+TechDomani@users.noreply.github.com>
2023-05-03 15:04:18 +0100

    Initial commit

---
+++ CarbonTracker.Data/CarbonTrackerDB.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Data;
+using System.Reflection;
+
+namespace CarbonTracker.Data
+{
+    public class CarbonTrackerDb
+    {
+        private string _connectionString;
+
+        public CarbonTrackerDb(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<string> GetUserNames()
+        {
+            var names = new List<string>();
+            using (SqliteConnection connection = new SqliteConnection())
+            {
+                connection.ConnectionString = _connectionString;
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = "Select * from main.users";
+                var dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    var name = dataReader.GetString(1);
+                    names.Add(name);
+                }
+            }
+            return names;
+        }
+
+        public void AddUserName(string userName)
+        {
+            using (SqliteConnection connection = new SqliteConnection())
+            {
+                connection.ConnectionString = _connectionString;
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = "insert into main.users (name) VALUES (@Name)";
+                var nameParameter = command.Parameters.Add("@Name", SqliteType.Text);
+                nameParameter.Value= userName;
+                command.ExecuteNonQuery();
+            }
+        }
+
+    }
+}
---
+++ CarbonTracker/Program.cs
@@ -0,0 +1,28 @@
+﻿using CarbonTracker.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace CarbonTracker
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appSettings.json").Build();
+            string connectionString = config["ConnectionStrings:carbonDb"] ?? string.Empty;
+            Console.WriteLine(connectionString);            
+            CarbonTrackerDb db = new(connectionString);
+
+            var names = db.GetUserNames();
+            Console.WriteLine("User Names:");
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter a name to add:");
+            string newName = Console.ReadLine() ?? string.Empty;
+            db.AddUserName(newName);
+        }
+    }
+}
